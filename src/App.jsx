5d357195f7c0c9e59d9815d15b22e3ae48ff0530
shameem7335-ship-it <shameem7335@gmail.com>
2026-04-01 21:40:09 +0600

    github push

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 
-import { useState } from 'react'
-import './App.css'
-import Banner from './Components/Banner/Banner'
-import Footer from './Components/Footer/Footer'
-import GetStart from './Components/GetStart/GetStart'
-import Navbar from './Components/Navbar/Navbar'
-import Pricing from './Components/Pricing/Pricing'
-import Products from './Components/Products/Products'
-import { ToastContainer } from 'react-toastify'
-
-
+import { useState } from 'react';
+import './App.css';
+import Banner from './Components/Banner/Banner';
+import Footer from './Components/Footer/Footer';
+import GetStart from './Components/GetStart/GetStart';
+import Navbar from './Components/Navbar/Navbar';
+import Pricing from './Components/Pricing/Pricing';
+import Products from './Components/Products/Products';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
 
-    const [cartItem, setCartItem] =useState([])
-
-
-
+    const [cartItem, setCartItem] =useState([]);
 
   return (
     <div>
